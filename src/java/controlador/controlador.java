bcd0ebad146fe705025c13c88e0d64f076a35159
angelica agudelo bautista <angelicaagudelobautista@gmail.com>
2024-08-23 11:39:14 -0500

    23agosto

--- src/java/controlador/controlador.java
+++ src/java/controlador/controlador.java
@@ -1,7 +1,7 @@
 
 package controlador;
 
-import confing.MetodosSQL;
+import modeloDAO.MetodosSQL;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
--- src/java/modeloDAO/MetodosSQL.java
+++ src/java/modeloDAO/MetodosSQL.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package confing;
+package modeloDAO;
 
+import confing.conexionBD;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
